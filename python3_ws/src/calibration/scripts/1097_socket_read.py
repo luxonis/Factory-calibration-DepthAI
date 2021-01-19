a7befe2a4258111a4c0388a0a6d350d42e6bbaef
saching13 <sachin.guruswamy@gmail.com>
2021-01-19 09:10:05 -0800

    removed start device from 1097

--- python3_ws/src/calibration/scripts/1097_socket_read.py
+++ python3_ws/src/calibration/scripts/1097_socket_read.py
@@ -176,18 +176,12 @@ class socket_calibration_node:
         # pygame.event.pump()
         return is_clicked
 
-    def start_device(self):
-        self.device = depthai.Device('', False)
-        # self.device = depthai.Device('/home/nuc/Desktop/depthai/.fw_cache/depthai-6fc8c54e33b8aa6d16bf70ac5193d10090dcd0d8.cmd', '')
-        self.pipeline = self.device.create_pipeline(self.config)
-        self.mx_id = self.device.get_mx_id()
-        # rospy.sleep(2)
-        # self.device.request_af_mode(depthai.AutofocusMode.AF_MODE_EDOF)
-        # self.device.request_af_mode(depthai.AutofocusMode.AF_MODE_AUTO)
-        # setting manual focus to rgb camera
-        # self.set_focus()
-        # self.set_focus()
-
+    # def start_device(self):
+    #     self.device = depthai.Device('', False)
+    #     # self.device = depthai.Device('/home/nuc/Desktop/depthai/.fw_cache/depthai-6fc8c54e33b8aa6d16bf70ac5193d10090dcd0d8.cmd', '')
+    #     self.pipeline = self.device.create_pipeline(self.config)
+    #     self.mx_id = self.device.get_mx_id()
+        
     def set_focus(self):
         if 1:
             print("RGB set_focus() disabled. TODO")
