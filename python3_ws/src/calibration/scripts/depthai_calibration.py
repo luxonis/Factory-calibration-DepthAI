1215fc28f54ac47481f01c6fe5a279b4b11f9fc7
Sachin Guruswamy <43363595+saching13@users.noreply.github.com>
2021-07-14 19:47:22 -0700

    Update depthai_calibration.py

--- python3_ws/src/calibration/scripts/depthai_calibration.py
+++ python3_ws/src/calibration/scripts/depthai_calibration.py
@@ -59,7 +59,7 @@ class depthai_calibration_node:
 
         self.focus_value = 135
         self.defaultLensPosition = 135
-        self.focusSigmaThreshold = 31
+        self.focusSigmaThreshold = 22
         if self.rgbCcm == 'Sunny':
             self.focus_value = 135
         elif self.rgbCcm == 'KingTop':
