74101070484ad37f7e6c98376d50063e806719a1
Sachin <sachin.guruswamy@gmail.com>
2021-12-07 18:10:56 -0800

    Updated threshold for focus

--- python3_ws/src/calibration/scripts/depthai_calibration.py
+++ python3_ws/src/calibration/scripts/depthai_calibration.py
@@ -96,7 +96,7 @@ class depthai_calibration_node:
 
         # self.focus_value = 0
         # self.defaultLensPosition = 135
-        self.focusSigmaThreshold = 25
+        self.focusSigmaThreshold = 40
         # if self.rgbCcm == 'Sunny':
         #     self.focus_value = 135
         # elif self.rgbCcm == 'KingTop':
