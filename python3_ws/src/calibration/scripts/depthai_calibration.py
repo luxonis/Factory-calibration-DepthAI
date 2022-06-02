89cd26524a194724d695c7a1088d9cd167bdcfd4
Sachin <sachin.guruswamy@gmail.com>
2022-06-01 22:58:12 -0700

    Updated focus test number

--- python3_ws/src/calibration/scripts/depthai_calibration.py
+++ python3_ws/src/calibration/scripts/depthai_calibration.py
@@ -107,7 +107,7 @@ class depthai_calibration_node:
 
         # self.focus_value = 0
         # self.defaultLensPosition = 135
-        self.focusSigmaThreshold = 30
+        self.focusSigmaThreshold = 25
         # if self.rgbCcm == 'Sunny':
         #     self.focus_value = 135
         # elif self.rgbCcm == 'KingTop':
