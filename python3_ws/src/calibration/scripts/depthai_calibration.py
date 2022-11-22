luxonis/Factory-calibration-DepthAI

commit fd71be10dc171f3966cffd851f3463d0b1394cc7

Removed in py3.6 unsupported dict union operator

python3_ws/src/calibration/scripts/depthai_calibration.py
@@ -1082,8 +1082,9 @@ class depthai_calibration_node:
             
 
     def upload_result(self, error=None):
-        self.result['tests'] = {k: v.is_checked() for k, v in self.auto_checkbox_dict.items()} \
-                        | {k: v.is_checked() for k, v in self.auto_focus_checkbox_dict.items()}
+        self.result['tests'] = {k: v.is_checked() for k, v in self.auto_checkbox_dict.items()}
+        self.result.update({k: v.is_checked() for k, v in self.auto_focus_checkbox_dict.items()})
+        
         production_support_server_api.add_result('calib', self.device_mxid, self.args['board'], self.bootloader_version, dai.__version__, self.camera_started_time, datetime.now(), self.result, error)
         production_support_server_api.sync()
 

commit 990d6ca2a25ea169934f6dc04f894495857d8f0d

Fixed a bug introduced by fd71be10dc171f3966cffd851f3463d0b1394cc7

python3_ws/src/calibration/scripts/depthai_calibration.py
@@ -1083,8 +1083,8 @@ class depthai_calibration_node:
 
     def upload_result(self, error=None):
         self.result['tests'] = {k: v.is_checked() for k, v in self.auto_checkbox_dict.items()}
-        self.result.update({k: v.is_checked() for k, v in self.auto_focus_checkbox_dict.items()})
-        
+        self.result['tests'].update({k: v.is_checked() for k, v in self.auto_focus_checkbox_dict.items()})
+
         production_support_server_api.add_result('calib', self.device_mxid, self.args['board'], self.bootloader_version, dai.__version__, self.camera_started_time, datetime.now(), self.result, error)
         production_support_server_api.sync()
 