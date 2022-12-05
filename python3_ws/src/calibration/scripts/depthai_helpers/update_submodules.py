42cf5bed0f8276b53f1cb2c46f87d5688eee14ee
Nejc Jezeršek <njezersek@gmail.com>
2022-12-05 20:49:31 +0100

    Remove --remote flag for submodule update

--- python3_ws/src/calibration/scripts/depthai_helpers/update_submodules.py
+++ python3_ws/src/calibration/scripts/depthai_helpers/update_submodules.py
@@ -3,4 +3,4 @@ import subprocess
 
 def update_submodules():
 	scriptDirectory = os.path.dirname(os.path.realpath(__file__))
-	subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive', '--remote'], cwd=scriptDirectory)
+	subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'], cwd=scriptDirectory)
