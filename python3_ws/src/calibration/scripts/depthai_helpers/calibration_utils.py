f2769b4c7d59a870e56fcd4ace755ed255c68058
saching13 <sachin.guruswamy@gmail.com>
2023-06-08 15:22:48 +0200

    Updated initial guess for intrinsic for fisheye

--- python3_ws/src/calibration/scripts/depthai_helpers/calibration_utils.py
+++ python3_ws/src/calibration/scripts/depthai_helpers/calibration_utils.py
@@ -368,7 +368,11 @@ class StereoCalibration(object):
         """
         Calibrates the camera using the dected corners.
         """
-        f = imsize[0] / (2 * np.tan(np.deg2rad(hfov/2)))
+        if hfov < 110:
+            f = imsize[0] / (2 * np.tan(np.deg2rad(hfov/2)))
+        else:
+            f = 500
+
         # TODO(sachin): Change the initialization to be initialized using the guess from fov
         print("CAMERA CALIBRATION")
         print(imsize)
